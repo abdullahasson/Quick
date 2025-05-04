264cd6518e963e074b39823f0cfe8a9857aaca9b
abdullahasson <bh532997@gmail.com>
2025-05-04 17:28:40 +0300

    add name for menus

--- sw.js
+++ sw.js
@@ -1,9 +1,32 @@
 const CACHE_NAME = 'cafe-menu-v1';
 const ASSETS = [
   '/',
-  '/index.html',
   '/styles.css',
-  '/cheesecake.jpg', // أضف كل الصور والملفات اللازمة
+  '/index.html',
+  '/candies.html',
+  '/cocktails.html',
+  '/cold-drinks.html',
+  '/hot-drinks.html',
+  '/milkshakes.html',
+  '/shisha.html',
+  '/icons/apple-icon-114x114.png',
+  '/icons/apple-icon-120x120.png',
+  '/icons/apple-icon-144x144.png',
+  '/icons/apple-icon-152x152.png',
+  '/icons/apple-icon-180x180.png',
+  '/icons/apple-icon-57x57.png',
+  '/icons/apple-icon-60x60.png',
+  '/icons/apple-icon-72x72.png',
+  '/icons/apple-icon-76x76.png',
+  '/icons/apple-icon.png',
+  '/icons/favicon-16x16.png',
+  '/icons/favicon-32x32.png',
+  '/icons/favicon-96x96.png',
+  '/icons/favicon.ico',
+  '/icons/icon-192x192.png',
+  '/icons/manifest.json',
+  '/icons/ms-icon-144x144.png',
+  '/icons/safari-pinned-tab.svg',
   // ... قم بإدراج جميع أصول الصفحة هنا
 ];
 
